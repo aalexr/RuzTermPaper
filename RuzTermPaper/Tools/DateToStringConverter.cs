81ab21345fa06051c5820eb1764b44f01e7e44fb
Alexander Rakitin <alex.rakitin@outlook.com>
2018-04-12 21:53:25 +0300

    removed using System

--- RuzTermPaper/Tools/DateToStringConverter.cs
+++ RuzTermPaper/Tools/DateToStringConverter.cs
@@ -1,17 +1,15 @@
-﻿using System;
-
-namespace RuzTermPaper.Tools
+﻿namespace RuzTermPaper.Tools
 {
     public class DateToStringConverter : Windows.UI.Xaml.Data.IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
+        public object Convert(object value, System.Type targetType, object parameter, string language)
         {
-            return ((DateTime)value).ToString("dd.MM");
+            return ((System.DateTime)value).ToString("dd.MM");
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        public object ConvertBack(object value, System.Type targetType, object parameter, string language)
         {
-            return DateTime.Parse((string)value);
+            return System.DateTime.Parse((string)value);
         }
     }
 }
